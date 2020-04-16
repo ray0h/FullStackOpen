96cf95eb9522f539d8b20d9d946a89c5045b4410
Raymond Oh <raymond.oh@gmail.com>
2020-04-16 11:10:40 -0400

    add solutions for Part 4, ex 4.1-4.2

---
+++ osa4_Submission/README.md
@@ -0,0 +1,3 @@
+# FullStackOpen Blog List Exercise Solutions
+
+[FullStackOpen ex. 4.1-4.2](https://fullstackopen.com/osa4/sovelluksen_rakenne_ja_testauksen_alkeet#tehtavat-4-1-4-2)
---
+++ osa4_Submission/app.js
@@ -0,0 +1,23 @@
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+
+mongoose.connect(config.mongoUrl, ({ useNewUrlParser : true, useUnifiedTopology : true }))
+    .then( () => {
+        logger.info('connected to MongoDB')
+    })
+    .catch(error => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
